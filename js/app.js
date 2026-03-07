98c85dfff835f547ac298d65333b601cc69f795c
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-03-07 22:50:59 +0800

    Update app.js
    Rev 20

--- js/app.js
+++ js/app.js
@@ -41,3 +41,65 @@ container.appendChild(card)
 })
 
 }
+
+const container=document.getElementById("channels")
+
+let channels=[]
+
+fetch(API_CHANNELS)
+
+.then(res=>res.json())
+
+.then(data=>{
+
+channels=data.channels
+
+renderChannels(channels)
+
+})
+
+
+
+function renderChannels(list){
+
+container.innerHTML=""
+
+list.forEach((ch,index)=>{
+
+const card=document.createElement("div")
+
+card.className="channel"
+
+card.innerHTML=`
+
+<div class="logo">📻</div>
+
+<h3>${ch.name}</h3>
+
+<p>${ch.artist}</p>
+
+<button class="fav">⭐</button>
+
+`
+
+card.onclick=()=>playChannel(ch,card,index)
+
+
+
+/* FAVORITE */
+
+card.querySelector(".fav").onclick=(e)=>{
+
+e.stopPropagation()
+
+saveFavorite(ch)
+
+}
+
+
+
+container.appendChild(card)
+
+})
+
+}
