18ab6cd34d9f95ab06c2e57e5768e78b1065e4c1
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-03-08 04:43:21 +0800

    Update app.js
    08032026

--- js/app.js
+++ js/app.js
@@ -3,18 +3,15 @@ const container=document.getElementById("channels")
 let channels=[]
 
 fetch(API_CHANNELS)
+
 .then(res=>res.json())
+
 .then(data=>{
 
-// support dua format JSON
-
-channels=data.channels || data
+channels=data.channels
 
 renderChannels(channels)
 
-})
-.catch(err=>{
-console.log("API ERROR",err)
 })
 
 
@@ -31,16 +28,10 @@ card.className="channel"
 card.innerHTML=`
 
 <h3>${ch.name}</h3>
+
 <p>${ch.artist}</p>
 
 `
-if(list.length===0){
-
-container.innerHTML="<p style='text-align:center'>Channel tidak tersedia</p>"
-
-return
-
-}
 
 card.onclick=()=>playChannel(ch,card,index)
 
